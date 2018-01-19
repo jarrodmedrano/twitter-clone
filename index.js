272e05fe2d154b14f36d5df9e4832fbbe0ece1de
jmedran <jmedran@gmail.com>
2018-01-19 13:09:23 -0700

    adding mongodb

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
-const app = express();
+const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+require('./services/passport');
+require('./models/User');
 
-app
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+mongoose.connect(keys.mongoURI);
+
+const app = express();
+require('./routes/authRoutes')(app);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
