9dbb6e88f67cbaa88a86325fd7d99141921d245c
jmedran <jmedran@gmail.com>
2018-01-23 14:54:01 -0700

    remove cruft

--- client/src/index.js
+++ client/src/index.js
@@ -17,6 +17,3 @@ ReactDOM.render(
   <Provider store={store}><App /></Provider>,
   document.querySelector(('#root'))
 );
-
-console.log('STRIPE_KEY_IS', process.env.REACT_APP_STRIPE_KEY);
-console.log('Environment is', process.env.NODE_ENV);
