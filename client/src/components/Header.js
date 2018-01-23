2e64f430417b194c2b0156803f9d5193fd8c0cd9
jmedran <jmedran@gmail.com>
2018-01-23 13:45:38 -0700

    redirect links

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
+  renderContent() {
+    switch(this.props.auth) {
+      case null:
+          return 'Still deciding';
+      case false:
+        return <a href="/auth/google">Login With Google</a>;
+      default:
+        return <a href="/api/logout">Logout</a>;
+    }
+  }
   render() {
     return (
         <nav>
           <div className="nav-wrapper">
-            <a href="/" className="left brand-logo">Emaily</a>
+            <Link to={} className="left brand-logo">Emaily</Link>
             <ul id="nav-mobile" className="right">
-              <li><a>Login With Google</a></li>
+              <li>{this.renderContent()}</li>
             </ul>
           </div>
         </nav>
