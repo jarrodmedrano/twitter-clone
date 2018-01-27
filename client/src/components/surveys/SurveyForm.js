3c20cb62994b5a033036ad98493463794cf5f5e5
jmedran <jmedran@gmail.com>
2018-01-27 15:16:39 -0700

    survey submit with router

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -26,10 +26,10 @@ class SurveyForm extends Component {
       <div>
         <form onSubmit={this.props.handleSubmit(this.props.onSurveySubmit)}>
           {this.renderFields()}
-          <Link to="/" className="red btn-flat white-text">
+          <Link to="/surveys" className="red btn-flat white-text">
             Cancel
           </Link>
-          <button type="submit" className="teal btn-flat white-text right">
+          <button type="submit" className="teal btn-flat right white-text">
             Next
             <i className="material-icons right">done</i>
           </button>
--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -2,12 +2,13 @@ import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
 import formFields from './formFields';
+import { withRouter } from 'react-router';
 import * as actions from '../../actions';
 
-const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) => {
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey, history }) => {
   const reviewFields = _.map(formFields, ({ name, label }) => {
     return (
-      <div>
+      <div key={name}>
         <label>{label}</label>
         <div>{formValues[name]}</div>
       </div>
@@ -19,17 +20,17 @@ const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) => {
       <h5>Please confirm your entries</h5>
       {reviewFields}
       <button
-        className="yellow btn-flat darken-3 white-text"
+        className="yellow darken-3 white-text btn-flat"
         onClick={onCancel}
       >
         Back
       </button>
       <button
-        onClick={submitSurvey(formValues)}
+        onClick={() => submitSurvey(formValues, history)}
         className="green btn-flat right white-text"
       >
         Send Survey
-        <i className="material-icons right white-text">email</i>
+        <i className="material-icons right">email</i>
       </button>
     </div>
   );
@@ -39,4 +40,4 @@ function mapStateToProps(state) {
   return { formValues: state.form.surveyForm.values };
 }
 
-export default connect(mapStateToProps, actions)(SurveyFormReview);
+export default connect(mapStateToProps, actions)(withRouter(SurveyFormReview));
