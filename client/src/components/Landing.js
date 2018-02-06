43d9ec2df37c627774acc073eb905b36e803799c
jmedran <jmedran@gmail.com>
2018-02-06 14:17:41 -0700

    changing homepage

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Landing = () => {
   return (
     <div style={{ textAlign: 'center' }}>
       <h1>Twitter Clone</h1>
-      Collect feedback from your users
+      <Link to="/tweets/new" className="btn blue darken-2">
+        Give a hoot, post a tweet
+      </Link>
     </div>
   );
 };
